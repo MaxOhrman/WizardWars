4df585b304955e2b919738f53394e1fb5fa2bafe
Max Öhrman <Ohrman.max@gmail.com>
2023-01-08 18:45:14 +0100

    - Moved fps counting code from the GamePanel - Comment cleanup

--- src/main/Game.java
+++ src/main/Game.java
@@ -6,6 +6,9 @@ public class Game implements Runnable {
     private GamePanel gamePanel;
     private Thread gameThread;
     private final int FPS_SET = 120; //The fps we want the game to run at
+    private int frameCount = 0;
+    private long lastCheck = 0;
+
 
     public Game() {
         this.gamePanel = new GamePanel();
@@ -21,15 +24,17 @@ public class Game implements Runnable {
         gameThread.start();
     }
 
-    // run will run at a separate thread
+    /**
+     * run will run at a separate thread
+     * 1 second is 1 000 000 000 nanoseconds.
+     * We divide it by the desired fps to get how often we want
+     * to repaint the scene. timePerFrame is the duration each frame should last
+     * if the desired time has elapsed(timePerFrame) then we draw a new frame
+     * repaint(); use GamePanels paintComponent to suggest it repaint the game scene.
+     */
     @Override
     public void run() {
 
-        // 1 second is 1 000 000 000 nanoseconds.
-        // We divide it by the desired fps to get how often we want
-        // to repaint the scene. timePerFrame is the duration each frame should last
-        // if the desired time has elapsed(timePerFrame) then we draw a new frame
-        // repaint(); use GamePanels paintComponent to suggest it repaint the game scene.
         double timePerFrame = 1000000000.0 / FPS_SET;
         long lastFrame = System.nanoTime();
         long now;
@@ -39,10 +44,29 @@ public class Game implements Runnable {
             if(now - lastFrame >= timePerFrame) {
                 gamePanel.repaint();
                 lastFrame = now;
+
+                //Increment frames for the fps counter
+                frameCount++;
             }
 
+            runFpsCounter();
         }
 
+
+    }
+
+    /**
+     * increment frames everytime we redraw the JPanel
+     * if 1000 ms has passed (1s) we update lastCheck to current time
+     * millis, we display the fps counter (redraws withing 1s)
+     * and then we start to count again by resetting frame counter.
+     */
+    private void runFpsCounter() {
+        if(System.currentTimeMillis() - lastCheck >= 1000)  {
+            lastCheck = System.currentTimeMillis();
+            System.out.println("FPS: " + frameCount);
+            frameCount = 0;
+        }
     }
 
 
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -11,8 +11,6 @@ public class GamePanel extends JPanel {
 
     private float xDelta = 100, yDelta = 100;
     private float xDir = 1f, yDir = 1f;
-    private int frames = 0;
-    private long lastCheck = 0;
     private Color color = new Color(150,20,90);
     private Random random;
 
@@ -38,10 +36,12 @@ public class GamePanel extends JPanel {
         this.yDelta = y;
     }
 
-    // paintComponent(Graphics g) is a part of JComponent that JPanel extends
-    // PaintComponent is also called when the system thinks it is needed for example:
-    // - Minimizing the window and bringing it back
-    // - Every resize of the frame containing the panel
+    /**
+     *   paintComponent(Graphics g) is a part of JComponent that JPanel extends
+     *   PaintComponent is also called when the system thinks it is needed for example:
+     * - Minimizing the window and bringing it back
+     * - Every resize of the frame containing the panel
+     */
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -49,19 +49,6 @@ public class GamePanel extends JPanel {
         updateRectangle();
         g.setColor(color);
         g.fillRect((int)xDelta,(int)yDelta,200,50);
-
-
-        //          :::::(FPS COUNTER):::::
-        // increment frames everytime we redraw the JPanel
-        // if 1000 ms has passed (1s) we update lastCheck to current time
-        // millis, we display the fps counter (redraws withing 1s)
-        // and then we start to count again by resetting frame counter.
-        frames++;
-        if(System.currentTimeMillis() - lastCheck >= 1000)  {
-            lastCheck = System.currentTimeMillis();
-            System.out.println("FPS: " + frames);
-            frames = 0;
-        }
 
     }
 
