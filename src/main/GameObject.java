850891ac4a3a83a3b578819d5eabb943668d60b4
Max Öhrman <Ohrman.max@gmail.com>
2023-01-08 20:50:12 +0100

    Added abstract GameObject with fields of velocity and x,y positions needed for future objects we will add to the game

---
+++ src/main/GameObject.java
@@ -0,0 +1,51 @@
+package main;
+
+import java.awt.*;
+
+public abstract class GameObject {
+
+    protected int x, y;
+    protected float velX = 0, velY = 0;
+
+    public GameObject(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public abstract void tick();
+    public abstract void render(Graphics g);
+    public abstract Rectangle getBounds();
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public float getVelX() {
+        return velX;
+    }
+
+    public void setVelX(float velX) {
+        this.velX = velX;
+    }
+
+    public float getVelY() {
+        return velY;
+    }
+
+    public void setVelY(float velY) {
+        this.velY = velY;
+    }
+
+}
