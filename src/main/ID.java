01ce3d652a2a1c032ad8369845557b0957cc474e
Max Öhrman <Ohrman.max@gmail.com>
2023-01-09 18:50:30 +0100

    Updated comments in Enum ID

--- src/main/ID.java
+++ src/main/ID.java
@@ -2,7 +2,13 @@ package main;
 
 /**
  * The ID Enum will list all game objects
- * of our game
+ * of our game.
+ *
+ * Whenever we create object
+ * we will also give it an appropriate id
+ * from enum ID. This is needed to identify
+ * gameObjects from one another
+ *
  */
 
 public enum ID {
